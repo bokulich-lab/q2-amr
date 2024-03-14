0e1d37acf8517f678316b2d3d636a1311244da68
VinzentRisch <100149044+VinzentRisch@users.noreply.github.com>
2024-03-14 11:32:32 +0100

    ENH: Added `CARDReadsKmerAnalysis` and `CARDMAGsKmerAnalysis` types (#48)

--- q2_amr/plugin_setup.py
+++ q2_amr/plugin_setup.py
@@ -39,6 +39,14 @@ from q2_amr.types._format import (
     CARDKmerDatabaseDirectoryFormat,
     CARDKmerJSONFormat,
     CARDKmerTXTFormat,
+    CARDMAGsKmerAnalysisDirectoryFormat,
+    CARDMAGsKmerAnalysisFormat,
+    CARDMAGsKmerAnalysisJSONFormat,
+    CARDReadsAlleleKmerAnalysisDirectoryFormat,
+    CARDReadsAlleleKmerAnalysisFormat,
+    CARDReadsGeneKmerAnalysisDirectoryFormat,
+    CARDReadsGeneKmerAnalysisFormat,
+    CARDReadsKmerAnalysisJSONFormat,
     CARDWildcardIndexFormat,
     GapDNAFASTAFormat,
 )
@@ -47,6 +55,9 @@ from q2_amr.types._type import (
     CARDAnnotation,
     CARDGeneAnnotation,
     CARDKmerDatabase,
+    CARDMAGsKmerAnalysis,
+    CARDReadsAlleleKmerAnalysis,
+    CARDReadsGeneKmerAnalysis,
 )
 
 citations = Citations.load("citations.bib", package="q2_amr")
@@ -221,6 +232,9 @@ plugin.register_semantic_types(
     CARDAnnotation,
     CARDAlleleAnnotation,
     CARDGeneAnnotation,
+    CARDReadsGeneKmerAnalysis,
+    CARDReadsAlleleKmerAnalysis,
+    CARDMAGsKmerAnalysis,
 )
 
 plugin.register_semantic_type_to_format(
@@ -239,11 +253,23 @@ plugin.register_semantic_type_to_format(
 plugin.register_semantic_type_to_format(
     SampleData[CARDGeneAnnotation], artifact_format=CARDGeneAnnotationDirectoryFormat
 )
-
+plugin.register_semantic_type_to_format(
+    SampleData[CARDReadsGeneKmerAnalysis],
+    artifact_format=CARDReadsGeneKmerAnalysisDirectoryFormat,
+)
+plugin.register_semantic_type_to_format(
+    SampleData[CARDReadsAlleleKmerAnalysis],
+    artifact_format=CARDReadsAlleleKmerAnalysisDirectoryFormat,
+)
+plugin.register_semantic_type_to_format(
+    SampleData[CARDMAGsKmerAnalysis],
+    artifact_format=CARDMAGsKmerAnalysisDirectoryFormat,
+)
 plugin.register_formats(
     CARDKmerDatabaseDirectoryFormat,
     CARDKmerJSONFormat,
     CARDKmerTXTFormat,
+    CARDMAGsKmerAnalysisDirectoryFormat,
     GapDNAFASTAFormat,
     CARDWildcardIndexFormat,
     CARDAnnotationTXTFormat,
@@ -256,6 +282,13 @@ plugin.register_formats(
     CARDAnnotationStatsFormat,
     CARDAlleleAnnotationDirectoryFormat,
     CARDGeneAnnotationDirectoryFormat,
+    CARDMAGsKmerAnalysisFormat,
+    CARDMAGsKmerAnalysisJSONFormat,
+    CARDReadsAlleleKmerAnalysisFormat,
+    CARDReadsGeneKmerAnalysisFormat,
+    CARDReadsKmerAnalysisJSONFormat,
+    CARDReadsGeneKmerAnalysisDirectoryFormat,
+    CARDReadsAlleleKmerAnalysisDirectoryFormat,
 )
 
 importlib.import_module("q2_amr.types._transformer")
