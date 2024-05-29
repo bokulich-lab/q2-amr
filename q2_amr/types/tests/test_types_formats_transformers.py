907afa5c4392992620c19d1f6770be70322ba366
VinzentRisch <risch.vinzent@gmail.com>
2024-05-29 12:43:30 +0200

    Revert "Merge branch '42_Genelengts_type' into 23_norm_tpm"
    This reverts commit ff80222daf58709be628527dc0e7a85db0806ffd, reversing
    changes made to 942cd8bced550afdaba03a0544b99bb9065d6207.

--- q2_amr/types/tests/test_types_formats_transformers.py
+++ q2_amr/types/tests/test_types_formats_transformers.py
@@ -339,37 +339,17 @@ class TestGeneLengthsTypesAndFormats(AMRTypesTestPluginBase):
         format = GeneLengthFormat(filepath, mode="r")
         format.validate()
 
-    def test_gene_length_format_validate_negative(self):
-        # Test ValidationErrors for wrong number of columns, wrong data type and
-        # duplicate gene names
-        test_cases = [
-            (
-                "gene_length_3_cols.txt",
-                " is not a(n) GeneLengthFormat file:\n\nThe file must consist of two "
-                "tab separated columns. The first column must be the gene names and "
-                "the second column must be the corresponding gene lengths.",
-            ),
-            (
-                "gene_length_switched.txt",
-                " is not a(n) GeneLengthFormat file:\n\nThe second column "
-                "(gene lengths) must be of type int or float.",
-            ),
-            (
-                "gene_length_duplicate.txt",
-                " is not a(n) GeneLengthFormat file:\n\nThere are no duplicate values "
-                "allowed in the first column (gene names).",
-            ),
-        ]
+    def test_gene_length_format_validate_negative_3_cols(self):
+        filepath = self.get_data_path("gene_length_3_cols.txt")
+        with self.assertRaises(ValidationError):
+            format = GeneLengthFormat(filepath, mode="r")
+            format.validate()
 
-        for file_name, expected_error_message in test_cases:
-            with self.subTest(file_name=file_name):
-                filepath = self.get_data_path(file_name)
-                with self.assertRaises(ValidationError) as context:
-                    format = GeneLengthFormat(filepath, mode="r")
-                    format.validate()
-                self.assertEqual(
-                    str(context.exception), filepath + expected_error_message
-                )
+    def test_gene_length_format_validate_negative_switched(self):
+        filepath = self.get_data_path("gene_length_switched.txt")
+        with self.assertRaises(ValidationError):
+            format = GeneLengthFormat(filepath, mode="r")
+            format.validate()
 
     def test_gene_length_directory_format_validate_positive(self):
         filepath = self.get_data_path("gene_length.txt")
@@ -377,21 +357,14 @@ class TestGeneLengthsTypesAndFormats(AMRTypesTestPluginBase):
         format = GeneLengthDirectoryFormat(self.temp_dir.name, mode="r")
         format.validate()
 
-    def test_card_allele_gene_annotation_dir_format_to_gene_length_dir_format(self):
-        transformations = [
-            (CARDAlleleAnnotationDirectoryFormat, GeneLengthDirectoryFormat),
-            (CARDGeneAnnotationDirectoryFormat, GeneLengthDirectoryFormat),
-        ]
+    def test_card_annotation_format_to_df_transformertt(self):
+        transformer = self.get_transformer(
+            CARDAlleleAnnotationDirectoryFormat, GeneLengthDirectoryFormat
+        )
 
-        for input_format, output_format in transformations:
-            with self.subTest(input_format=input_format, output_format=output_format):
-                transformer = self.get_transformer(input_format, output_format)
-                annotation = input_format(
-                    self.get_data_path("annotate_reads_output"), "r"
-                )
-                obs = transformer(annotation)
-                self.assertIsInstance(obs, GeneLengthDirectoryFormat)
-                format = GeneLengthFormat(
-                    os.path.join(obs.path, "gene_length.txt"), mode="r"
-                )
-                format.validate()
+        annotation = CARDAlleleAnnotationDirectoryFormat(
+            self.get_data_path("annotate_reads_output"), "r"
+        )
+
+        obs = transformer(annotation)
+        self.assertIsInstance(obs, GeneLengthDirectoryFormat)
