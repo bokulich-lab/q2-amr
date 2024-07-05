4e38e218f28d7eb0d0e70f45993ab97c3c5c2514
VinzentRisch <risch.vinzent@gmail.com>
2024-07-05 15:01:54 +0200

    working action with non nested output format

--- q2_amr/amrfinderplus/mags.py
+++ q2_amr/amrfinderplus/mags.py
@@ -41,6 +41,10 @@ def annotate_mags_amrfinderplus(
     with tempfile.TemporaryDirectory() as tmp:
         for samp_mag in list(manifest.index):
             input_sequence = manifest.loc[samp_mag, "filename"]
+
+            sample_id = samp_mag[0]
+            mag_id = samp_mag[1]
+
             run_amrfinderplus_n(
                 tmp,
                 amrfinderplus_db,
@@ -52,12 +56,12 @@ def annotate_mags_amrfinderplus(
                 coverage_min,
                 translation_table,
                 threads,
-                samp_mag[1],
+                mag_id,
             )
 
             frequency_df = read_in_txt(
-                path=os.path.join(tmp, "amr_annotations.tsv"),
-                samp_bin_name=str(os.path.join(samp_mag[0], samp_mag[1])),
+                path=os.path.join(tmp, f"{mag_id}_amr_annotations.tsv"),
+                samp_bin_name=str(os.path.join(sample_id, mag_id)),
                 data_type="mags",
                 colname="Gene symbol",
             )
@@ -65,13 +69,13 @@ def annotate_mags_amrfinderplus(
             for dir_format, file_name in zip(
                 [annotations, mutations, genes],
                 [
-                    "amr_annotations.tsv",
-                    "amr_mutations.tsv",
-                    f"{samp_mag[1]}_amr_genes.fasta",
+                    f"{mag_id}_amr_annotations.tsv",
+                    f"{mag_id}_amr_mutations.tsv",
+                    f"{mag_id}_amr_genes.fasta",
                 ],
             ):
                 if dir_format in [annotations, mutations]:
-                    des_dir = os.path.join(str(dir_format), samp_mag[0], samp_mag[1])
+                    des_dir = os.path.join(str(dir_format), sample_id)
                     os.makedirs(des_dir, exist_ok=True)
                 else:
                     des_dir = str(dir_format)
@@ -108,12 +112,12 @@ def run_amrfinderplus_n(
         "--database",
         str(amrfinderplus_db),
         "-o",
-        f"{tmp}/amr_annotations.tsv",
+        f"{tmp}/{mag_id}_amr_annotations.tsv",
         "--print_node",
         "--nucleotide_output",
         f"{tmp}/{mag_id}_amr_genes.fasta",
         "--mutation_all",
-        f"{tmp}/amr_mutations.tsv",
+        f"{tmp}/{mag_id}_amr_mutations.tsv",
     ]
     if threads:
         cmd.extend(["--threads", str(threads)])
