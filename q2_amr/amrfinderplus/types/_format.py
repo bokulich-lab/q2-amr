ee10a37916e7928b7e376df10b14f4c263e61ff2
VinzentRisch <risch.vinzent@gmail.com>
2024-07-11 11:59:59 +0200

    removed changes cds and version format files

--- q2_amr/amrfinderplus/types/_format.py
+++ q2_amr/amrfinderplus/types/_format.py
@@ -27,8 +27,6 @@ class AMRFinderPlusDatabaseDirFmt(model.DirectoryFormat):
     amrprot_mutation = model.File("AMRProt-mutation.tab", format=TextFormat)
     amrprot_suppress = model.File("AMRProt-suppress", format=TextFormat)
     amrprot_susceptible = model.File("AMRProt-susceptible.tab", format=TextFormat)
-    changes = model.File("changes.txt", format=TextFormat)
-    db_version = model.File("database_format_version.txt", format=TextFormat)
     fam = model.File("fam.tab", format=TextFormat)
     taxgroup = model.File("taxgroup.tab", format=TextFormat)
     version = model.File("version.txt", format=TextFormat)
@@ -39,8 +37,6 @@ class AMRFinderPlusDatabaseDirFmt(model.DirectoryFormat):
         r"^AMR_DNA-[a-zA-Z_]+\.n..$", format=BinaryFormat
     )
     amr_dna_tab = model.FileCollection(r"^AMR_DNA-[a-zA-Z_]+\.tab$", format=TextFormat)
-    amr_cds_comp = model.FileCollection(r"^AMR_CDS\.n..$", format=BinaryFormat)
-    amr_cds = model.File("AMR_CDS", format=MixedCaseDNAFASTAFormat)
 
     @amr_lib_comp.set_path_maker
     def amr_lib_comp_path_maker(self, extension):
@@ -57,10 +53,6 @@ class AMRFinderPlusDatabaseDirFmt(model.DirectoryFormat):
     @amr_dna_comp.set_path_maker
     def amr_dna_comp_path_maker(self, species, extension):
         return "AMR_DNA-%s.%s" % species, extension
-
-    @amr_cds_comp.set_path_maker
-    def amr_cds_comp_path_maker(self, extension):
-        return "AMR_CDS.%s" % extension
 
     @amr_dna_tab.set_path_maker
     def amr_dna_tab_path_maker(self, species):
