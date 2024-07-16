38e48b59601142f50503b8f28bc455b370ad02fe
VinzentRisch <100149044+VinzentRisch@users.noreply.github.com>
2024-07-16 16:57:40 +0200

    ENH: Added new action that can download AMRFinderPlus database called `fetch_amrfinderplus_db`  (#82)

--- q2_amr/plugin_setup.py
+++ q2_amr/plugin_setup.py
@@ -29,6 +29,7 @@ from qiime2.core.type import (
 from qiime2.plugin import Citations, Plugin
 
 from q2_amr import __version__
+from q2_amr.amrfinderplus.database import fetch_amrfinderplus_db
 from q2_amr.amrfinderplus.types._format import (
     AMRFinderPlusAnnotationDirFmt,
     AMRFinderPlusAnnotationFormat,
@@ -1079,6 +1080,20 @@ plugin.methods.register_function(
     description="With kmer_build_card a kmer database can be built with a custom kmer."
     " size",
     citations=[citations["alcock_card_2023"]],
+)
+plugin.methods.register_function(
+    function=fetch_amrfinderplus_db,
+    inputs={},
+    parameters={},
+    outputs=[("amrfinderplus_db", AMRFinderPlusDatabase)],
+    input_descriptions={},
+    parameter_descriptions={},
+    output_descriptions={
+        "amrfinderplus_db": "AMRFinderPlus database.",
+    },
+    name="Download AMRFinderPlus database.",
+    description="Download the latest version of the AMRFinderPlus database.",
+    citations=[citations["feldgarden2021amrfinderplus"]],
 )
 
 # Registrations
